431ab1c6d6fdae90384ca4037b788b0f382860af
Shailendra <sahushailendrakumar053@gmail.comgit config --global user.name shailendra24sahu>
2023-06-13 16:06:03 +0530

    second push on branch1

--- test.py
+++ test.py
@@ -6,8 +6,20 @@ from flask_cors import CORS, cross_origin
 # from prediction_Validation_Insertion import pred_validation
 from trainingModel import trainModel
 from training_Validation_Insertion import train_validation
+from DataTypeValidation_Insertion_Training import DataTypeValidation
+# from DataTypeValidation_Insertion_Training.DataTypeValidation import DataTypeValidation
 #import flask_monitoringdashboard as dashboard
 # from predictFromModel import prediction
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from data_ingestion import data_loader
+from data_preprocessing import preprocessing
+from data_preprocessing import clustering
+from best_model_finder import tuner
+from file_operations import file_methods
+from application_logging import logger
+
 
 # os.putenv('LANG', 'en_US.UTF-8')
 # os.putenv('LC_ALL', 'en_US.UTF-8')
